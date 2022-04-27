1c4b3b5a711b0e205b8ae0a093a118106a150e9e
T1anhen9 <78781292+RaRaRaRabbittt@users.noreply.github.com>
2022-04-27 17:31:47 -0400

    Add files via upload

---
+++ operation.py
@@ -0,0 +1,73 @@
+import requests
+
+class operation:
+    def __init__(self):
+        self.files = [
+        ]
+        self.headers = {
+            'x-api-key': 'ca8eb275449c03fd1f5f',
+            'userId': '1111',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'PostmanRuntime/7.29.0'
+        }
+
+    def get_runs(self, type, teamId, count):
+        str1 = "https://www.notexponential.com/aip2pgaming/api/rl/score.php?type="
+        str2 = "&teamId="
+        str3 = "&count="
+        total = str1, type, str2, teamId, str3, count
+
+        url = str.join(total)
+
+        payload = {
+        }
+
+        response = requests.request("GET", url, headers=self.headers, data=payload, files=self.files)
+        return response
+
+    def get_location(self, type, teamId):
+        str1 = "https://www.notexponential.com/aip2pgaming/api/rl/gw.php?type="
+        str2 = "&teamId="
+        total = str1, type, str2, teamId
+
+        url = str.join(total)
+
+        payload = {
+        }
+
+        response = requests.request("GET", url, headers=self.headers, data=payload, files=self.files)
+        return response
+
+    def enter_a_world(self, teamId, type, worldId):
+        url = "https://www.notexponential.com/aip2pgaming/api/rl/gw.php"
+
+        payload = {'teamId': teamId,
+                   'type': type,
+                   'worldId': worldId}
+
+        response = requests.request("POST", url, headers=self.headers, data=payload, files=self.files)
+        return response
+
+    def make_a_move(self, teamId, type, worldId, move):
+        url = "https://www.notexponential.com/aip2pgaming/api/rl/gw.php"
+
+        payload = {'move': move,
+                   'teamId': teamId,
+                   'type': type,
+                   'worldId': worldId}
+
+        response = requests.request("POST", url, headers=self.headers, data=payload, files=self.files)
+        return response
+
+    def get_score(self, type, teamId):
+        str1 = "https://www.notexponential.com/aip2pgaming/api/rl/score.php?type="
+        str2 = "&teamId="
+        total = str1, type, str2, teamId
+
+        url = str.join(total)
+
+        payload = {
+        }
+
+        response = requests.request("GET", url, headers=self.headers, data=payload, files=self.files)
+        return response
