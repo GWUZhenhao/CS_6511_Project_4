e9bf50224dcd652c9457618992816d1cee2e9faf
36394 <zzhao98@gwu.edu>
2022-05-02 10:18:25 -0400

    visualize the q_table (done).

--- running_gridworld.py
+++ running_gridworld.py
@@ -2,29 +2,55 @@ import pickle
 import tkinter as tk
 import operation
 import gridworld
+import matplotlib.pyplot as plt
+import time
 
-def print_q_table(q_table):
+UNIT = 50
+HEIGHT = 40
+WIDTH = 40
+
+class Env(tk.Tk):
+    def __init__(self):
+        super(Env, self).__init__()
+        self.action_space = ['u', 'd', 'l', 'r']
+        self.n_actions = len(self.action_space)
+        self.title('Q Learning')
+        self.canvas = self._build_canvas()
+        self.texts = []
+
+    def _build_canvas(self):
+        canvas = tk.Canvas(self, bg='white',
+                           height=HEIGHT * UNIT,
+                           width=WIDTH * UNIT)
+
+        for c in range(0, WIDTH * UNIT, UNIT):
+            x0, y0, x1, y1 = c, 0, c, HEIGHT * UNIT
+            canvas.create_line(x0, y0, x1, y1)
+        for r in range(0, HEIGHT * UNIT, UNIT):
+            x0, y0, x1, y1 = 0, r, HEIGHT * UNIT, r
+            canvas.create_line(x0, y0, x1, y1)
+
+
+        canvas.pack()
+
+        return canvas
+
+    def render(self):
+        time.sleep(0.5)
+        self.update()
+
+
+def print_q_table(q_table, env):
 
     # Creat an empty canvas with 40*40 grid
-    UNIT = 100
-    HEIGHT = 40
-    WIDTH = 40
-    canvas = tk.Canvas(bg='white',
-                       height=HEIGHT * UNIT,
-                       width=WIDTH * UNIT)
-    for c in range(0, WIDTH * UNIT, UNIT):
-        x0, y0, x1, y1 = c, 0, c, HEIGHT * UNIT
-        canvas.create_line(x0, y0, x1, y1)
-    for r in range(0, HEIGHT * UNIT, UNIT):
-        x0, y0, x1, y1 = 0, r, HEIGHT * UNIT, r
-        canvas.create_line(x0, y0, x1, y1)
-    canvas.pack()
 
     # For each grid, draw the four q value
-    size = 10
+    size = 5
     style = 'normal'
     anchor = "nw"
     font = 'Helvetica'
+    for i in env.texts:
+        env.canvas.delete(i)
     for i in range(HEIGHT):
         for j in range(WIDTH):
             for action in range(0, 4):
@@ -32,18 +58,20 @@ def print_q_table(q_table):
                 if str(state) in q_table.keys():
                     temp = q_table[str(state)][action]
                     if action == 0:
-                        origin_x, origin_y = 7, 42
+                        origin_x, origin_y = 4, 21
                     elif action == 1:
-                        origin_x, origin_y = 85, 42
+                        origin_x, origin_y = 43, 21
                     elif action == 2:
-                        origin_x, origin_y = 42, 5
+                        origin_x, origin_y = 21, 3
                     else:
-                        origin_x, origin_y = 42, 77
+                        origin_x, origin_y = 21, 39
 
                     x, y = origin_y + (UNIT * i), origin_x + (UNIT * j)
                     font = (font, str(size), style)
-                    canvas.create_text(int(x), int(y), fill="black", text=round(temp, 2), font=font, anchor=anchor)
+                    text = env.canvas.create_text(int(x), int(y), fill="black", text=round(temp, 3), font=font, anchor=anchor)
 
+                    env.texts.append(text)
+    env.canvas.update()
     print()
 
 
@@ -54,6 +82,7 @@ if __name__ == '__main__':
     agent = gridworld.q_learning([0, 1, 2, 3])
     op = operation.operation(teamId=teamId)
     actions = ['N', 'S', 'W', 'E']
+    env = Env()
 
     # Reset the game at first
     op.reset_my_team()
@@ -94,7 +123,7 @@ if __name__ == '__main__':
             # Another way to stop
             # if int(op.get_location()['world']) == -1:
             #     break
-            print_q_table(agent.q_table)
+            print_q_table(agent.q_table, env)
         print(agent.q_table)
 
     # Save the q_table we trained
