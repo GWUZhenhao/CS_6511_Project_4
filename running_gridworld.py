24108092bf39b34be4e3a6a52e72988c7ddef3d2
36394 <zzhao98@gwu.edu>
2022-05-01 21:14:07 -0400

    visualize the q_table.

--- running_gridworld.py
+++ running_gridworld.py
@@ -1,6 +1,51 @@
+import pickle
+import tkinter as tk
 import operation
 import gridworld
-import json
+
+def print_q_table(q_table):
+
+    # Creat an empty canvas with 40*40 grid
+    UNIT = 100
+    HEIGHT = 40
+    WIDTH = 40
+    canvas = tk.Canvas(bg='white',
+                       height=HEIGHT * UNIT,
+                       width=WIDTH * UNIT)
+    for c in range(0, WIDTH * UNIT, UNIT):
+        x0, y0, x1, y1 = c, 0, c, HEIGHT * UNIT
+        canvas.create_line(x0, y0, x1, y1)
+    for r in range(0, HEIGHT * UNIT, UNIT):
+        x0, y0, x1, y1 = 0, r, HEIGHT * UNIT, r
+        canvas.create_line(x0, y0, x1, y1)
+    canvas.pack()
+
+    # For each grid, draw the four q value
+    size = 10
+    style = 'normal'
+    anchor = "nw"
+    font = 'Helvetica'
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            for action in range(0, 4):
+                state = [i, j]
+                if str(state) in q_table.keys():
+                    temp = q_table[str(state)][action]
+                    if action == 0:
+                        origin_x, origin_y = 7, 42
+                    elif action == 1:
+                        origin_x, origin_y = 85, 42
+                    elif action == 2:
+                        origin_x, origin_y = 42, 5
+                    else:
+                        origin_x, origin_y = 42, 77
+
+                    x, y = origin_y + (UNIT * i), origin_x + (UNIT * j)
+                    font = (font, str(size), style)
+                    canvas.create_text(int(x), int(y), fill="black", text=round(temp, 2), font=font, anchor=anchor)
+
+    print()
+
 
 if __name__ == '__main__':
 
@@ -49,6 +94,10 @@ if __name__ == '__main__':
             # Another way to stop
             # if int(op.get_location()['world']) == -1:
             #     break
-
+            print_q_table(agent.q_table)
         print(agent.q_table)
+
+    # Save the q_table we trained
+    with open("q_table.pkl", "wb") as pkl_handle:
+        pickle.dump(agent.q_table, pkl_handle)
     print('finished')
